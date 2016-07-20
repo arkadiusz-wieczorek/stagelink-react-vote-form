c59b3bc2478bc40e8be360c68c2b6733fdcda075
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-07-20 13:13:52 +0200

    Add class to html

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # browserify ./components/vote-form.jsx -o ./public/js/bundle.js -t [ babelify --presets [ es2015 react ] ]
-watchify ./components/vote-form.jsx -o ./public/js/bundle.js -t [ babelify --presets [ es2015 react ] ] --verbose
+watchify ./components/vote-iframe.jsx -o ./public/js/bundle.js -t [ babelify --presets [ es2015 react ] ] --verbose
