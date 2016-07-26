89067aa22c4ed325b918763476a8004b5c41e586
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-07-26 22:35:01 +0200

    Move build script to root dir

--- app/build.sh
+++
@@ -1,2 +0,0 @@
-# browserify ./components/vote-form.jsx -o ./public/js/bundle.js -t [ babelify --presets [ es2015 react ] ]
-watchify ./components/index.jsx -o ./public/js/bundle.js -t [ babelify --presets [ es2015 react ] ] --verbose
