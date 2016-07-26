f772888da2b6d0dd1f93c1959b37b5f22305a429
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-07-26 22:42:26 +0200

    Rewrite header and footer components to stateless components

--- app/components/vote-header.jsx
+++ app/components/vote-header.jsx
@@ -1,18 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
-
-class VoteHeader extends React.Component{
-    constructor(props){
-        super(props)
-    }
-
-    render () {
-        return (
-            <header>
-                <h1>{this.props.artist_name}, come to my town!</h1>
-            </header>
-        )
-    }
-}
-
+const VoteHeader = (props) => (
+    <header><h1>{props.artist_name}, come to my town!</h1></header>
+)
 export default VoteHeader
