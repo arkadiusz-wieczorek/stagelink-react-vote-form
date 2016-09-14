3902048623a387f34ca9c7f372983e5140c04d33
Arkadiusz Wieczorek <kontakt@arkadiusz-wieczorek.pl>
2016-09-14 19:33:38 +0200

    Move footer.jsx

---
+++ lib/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = (props) => (
+	<footer>
+		<div className="fragment__vote-about">
+			<p>Your vote does not commit you to buy a ticket</p>
+		</div>
+		<span className="icon icon-stagelink_icon" />
+			<p>Made with <span className="icon icon-heart" /> in Berlin &amp; New York</p>
+			<p>© Stagelink GmbH 2016</p>
+	</footer>
+)
+
+export default Footer;
